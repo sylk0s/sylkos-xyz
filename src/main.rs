5afb090ffdd87727e1dce41230af4fdc7a4298cd
Sylkos <sylkos49@gmail.com>
2024-05-25 19:16:23 -0400

    fixed the stupid stuff in the cargo toml

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use dioxus_router::prelude::*;
 use sylkos_xyz::Route;
 
 // Urls are relative to your Cargo.toml file
-const _TAILWIND_URL: &str = manganis::mg!(file("public/tailwind.css"));
+// const _TAILWIND_URL: &str = manganis::mg!(file("public/tailwind.css"));
 
 fn main() {
     launch(App);
